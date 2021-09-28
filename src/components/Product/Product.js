edc8bdb95630e8115034fbeea271c230078c5e31
unknown <mdmehedyh55@gamil.com>
2021-09-28 14:09:46 +0600

    add-34-14

---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    // console.log(props.product);
+    const {img, name, seller, price, stock} = props.product;
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div style={{marginLeft: '10px'}}>
+                <h4 className="product-name">{name}</h4>
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <br />
+                <p><small>Only {stock} left in stock - Order soon</small></p>
+                <button onClick={()=> props.handleAddProduct(props.product)} className="main-button"><FontAwesomeIcon icon={faCartArrowDown}/> add to cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
